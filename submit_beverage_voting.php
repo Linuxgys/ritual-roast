cbabaca1bcac7bc02480ccc5199acef8581e5de2
L!nuxGeek <99080650+Linuxgys@users.noreply.github.com>
2025-03-27 19:54:10 +0530

    Update submit_beverage_voting.php

--- submit_beverage_voting.php
+++ submit_beverage_voting.php
@@ -15,7 +15,7 @@ if ($name === '' || $email === '' || $beverage === '') {
         echo json_encode(array('message' => 'Error on submit data', 'status' => 'error', 'sql_error' => mysqli_error($connection)));
         die();
     } else {
-        echo json_encode(array('message' => 'You're awesome! 🎉 Thank you for choosing us !', 'status' => 'success'));
+        echo json_encode(array('message' => 'You are awesome Thank for choosing us', 'status' => 'success'));
         die();
     }
 }
